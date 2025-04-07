24cd2737259a04129220e782e52410305c5883fc
SanteusanioAbru <asanteusanio@mail.austral.edu.ar>
2025-04-07 17:13:57 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,7 +1,4 @@
 def slice_advanced():
-    texto = "Awesome"
-    medio = texto[3-1:3+2]
-    print(texto.lower()[0:3])
-    print(medio)
-    print(texto.lower()[0:8])
-slice_simple()
+    texto=input("Ingrese un texto")
+    print(f"{texto[5::2]}")
+slice_advanced() 
