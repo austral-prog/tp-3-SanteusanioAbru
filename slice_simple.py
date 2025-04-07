da79e291ab91579fae3eacdcb460ec8ea1fdd640
SanteusanioAbru <asanteusanio@mail.austral.edu.ar>
2025-04-07 12:43:42 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,7 @@
 def slice_simple():
     texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    medio = texto[3-1:3+2]
+    print(texto.lower()[0:3])
+    print(medio)
+    print(texto.lower()[0:8])
+slice_simple()
